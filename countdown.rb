e008c050c8f12be848fbb50d24c53ecd1e2561d5
Alex S <alex.sunga@fliptech.co>
2018-12-01 10:45:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown (starting_number)
+  phrase = "HAPPY NEW YEAR!"
+  starting_number = 10 
+  
+  while starting_number !=0 
+  puts "#{starting_number} SECOND(S)!"
+  starting_number -=1 
+  
+  end 
+  phrase 
 end
+
+def countdown_with_sleep(starting_number)
+  phrase = "HAPPY NEW YEAR!"
+  starting_number = 10 
+  
+  while starting_number !=0 
+    puts "#{starting_number} SECOND(S)!"
+    sleep(1)
+    starting_number -=1 
+  end
+  phrase 
+end 
